cc48c330c12a7e1550d2e91d4bbf1604e6692f0b
Alexandr <alexpryp@gmail.com>
2022-09-23 19:38:57 +0300

    ADD | add first part of the project benefits section

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,6 +134,55 @@ const Home: NextPage = () => {
             </div>
           </div>
         </section>
+        <section className={styles.benefits_section}>
+          <div className={styles.benefits_container}>
+            <h2 className={styles.benefits_title}>
+              Project benefits
+            </h2>
+            <div className={styles.benefits_first_block}>
+              <div className={styles.benefits_text_container}>
+                <div className={styles.benefits_text_block}>
+                  <div className={styles.benefits_number_container}>
+                    <span className={styles.benefits_number}>01</span>
+                  </div>
+                  <h3 className={styles.benefits_text_title}>NFT</h3>
+                  <div>
+                    <p className={styles.benefits_text}>
+                    Trading on Planetex is quite simple and requires only signing 
+                    up for a Planetex account and wallet creation. Keep track of 
+                    your orders in real time with in depth market analysis. 
+                    Increase your chance of winning your open contracts. We 
+                    provided with you all the tools you need.
+                    </p>
+                  </div>
+                </div>
+                <div className={`${styles.benefits_text_block}, ${styles.benefits_text_block2}`}>
+                  <div className={styles.benefits_number_container}>
+                    <span className={styles.benefits_number}>02</span>
+                  </div>
+                  <h3 className={styles.benefits_text_title}>Staking</h3>
+                  <div>
+                    <p className={styles.benefits_text}>
+                    Trading on Planetex is quite simple and requires only signing 
+                    up for a Planetex account and wallet creation. Keep track of 
+                    your orders in real time with in depth market analysis. 
+                    Increase your chance of winning your open contracts. We 
+                    provided with you all the tools you need.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.benefits_img_container}>
+                <Image 
+                  src="/images/Group357965.png"
+                  width={725}
+                  height={746}
+                  alt="Coin"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
         
         
         
