03eee1c560d57873d7c2ed19700edfa9330ba656
Alexandr <alexpryp@gmail.com>
2022-09-27 21:01:16 +0300

    ADD | roadmap section

--- pages/index.tsx
+++ pages/index.tsx
@@ -396,6 +396,50 @@ const Home: NextPage = () => {
             />
           </div>
         </section>
+        <section className={styles.roadmap_section}>
+          <h2 className={styles.roadmap_title}>
+            Roadmap
+          </h2>
+          <div className={styles.roadmap_content}>
+            <div className={styles.roadmap_text_block}>
+              <div className={styles.roadmap_text_subtitle}>
+                NFT
+              </div>
+              <div className={styles.roadmap_text}>
+                <p>
+                Echoing Astar Network&apos;s vision to be the heart of the multi-chain 
+                future, Starfish aims Echoing Astar Network&apos;s vision to be the 
+                heart of the multi-chain future, Starfish aims Echoing Astar 
+                Network&apos;s vision to be the heart of the, Starfish aims Echoing 
+                Astar Network&apos;s vision to be the heart of the multi-chain future, 
+                Starfish aims Echoing Astar Network&apos;s vision to be the heart of 
+                the multi-chain future, Starfish aimsing Astar Network&apos;s vision 
+                to be the heart of the multi-chain future, Starfish aims 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.roadmap_scale_container}>
+            <div className={styles.roadmap_quarters}>
+              <div className={styles.roadmap_quarter}>
+                Quarter 1 2023
+              </div>
+              <div className={styles.roadmap_quarter}>
+                Quarter 2 2023
+              </div>
+              <div className={styles.roadmap_quarter}>
+                Quarter 3 2023
+              </div>
+              <div className={styles.roadmap_quarter}>
+                Quarter 4 2023
+              </div>
+            </div>
+            <div className={styles.roadmap_scale}>
+              <div className={styles.roadmap_internal}>
+              </div>
+            </div>
+          </div>
+        </section>
 
 
 
