64ad6e2181f05392fc8ad3d6015e24034d98608c
Alexandr <alexpryp@gmail.com>
2022-10-03 16:51:34 +0300

    ADD | Footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -712,6 +712,9 @@ const Home: NextPage = () => {
 
 
         <section className={styles.partners_section}>
+          <h2 className={styles.partners_title}>
+            Partners
+          </h2>
           <div className={styles.partners_block}>
             <div className={styles.partners_item}>
               <div className={styles.partners_logo}>
@@ -780,62 +783,67 @@ const Home: NextPage = () => {
             </div>
           </div>
         </section>
-        
-        
-        
-        
-        
-        {/* <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <div className={styles.footer_content}>
+          <div className={styles.footer_contacts}>
+            <h3 className={styles.footer_title}>
+              Contact us
+            </h3>
+            <div className={styles.footer_text_medium}>
+              <div>
+                Listing: listing@planetex.io
+              </div>
+              <div>
+                Support: support@planetex.io
+              </div>
+              <div>
+                Parthership: info@planetex.io
+              </div>
+            </div>
+          </div>
+          <div className={styles.footer_products_about}>
+            <div className={styles.footer_products}>
+              <h3 className={styles.footer_title}>
+                Products
+              </h3>
+              <div className={styles.footer_text_regular}>
+                <div>
+                Trading
+                </div>
+                <div>
+                Exchange
+                </div>
+                <div>
+                Launchpad
+                </div>
+                <div>
+                Stake Box
+                </div>
+              </div>
+            </div>
+            <div className={styles.footer_products}>
+              <h3 className={styles.footer_title}>
+                About us
+              </h3>
+              <div className={styles.footer_text_regular}>
+                <div>
+                  About Planetex
+                </div>
+                <div>
+                  Blog
+                </div>
+                <div>
+                  Terms
+                </div>
+                <div>
+                  Privacy
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   )
