cc70d2608ef3d16fee7b55b5f37b87af53cd4b4a
Alexandr <alexpryp@gmail.com>
2022-10-10 04:55:57 +0300

    EDIT | edit slider in the product suite container

--- components/ProductSlider/ProductSlider.tsx
+++ components/ProductSlider/ProductSlider.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React, { Component } from "react"
 import Slider from "react-slick"
 import styles from './ProductSlider.module.css'
@@ -12,29 +13,116 @@ export default class ProductSlider extends Component {
       infinite: true,
       centerPadding: "60px",
       slidesToShow: 1,
-      speed: 500
+      speed: 500,
+      arrows: false,
+      variableWidth: true
     };
     return (
-      <div className={styles.product_slider_container}>
-        <h2>Center Mode</h2>
+      <div className={styles.product_slider_container_mobile}>
         <Slider {...settings}>
-          <div className={styles.product_slider_slide}>
-            <h3>1</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/swap.png"
+                  width={55}
+                  height={52}
+                  alt="Swap icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Swap</p>
+            </div>
           </div>
-          <div className={styles.product_slider_slide}>
-            <h3>2</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/staking.png"
+                  width={59}
+                  height={56}
+                  alt="Staking icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Staking</p>
+            </div>
           </div>
-          <div className={styles.product_slider_slide}>
-            <h3>3</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/trading.png"
+                  width={49}
+                  height={49}
+                  alt="Trading icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Trading</p>
+            </div>
           </div>
-          <div className={styles.product_slider_slide}>
-            <h3>4</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/launchpad.png"
+                  width={52}
+                  height={52}
+                  alt="Launchpad icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Launchpad</p>
+            </div>
           </div>
-          <div className={styles.product_slider_slide}>
-            <h3>5</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon} style={{}}>
+                <Image 
+                  src="/images/swap.png"
+                  width={55}
+                  height={52}
+                  alt="Swap icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Swap</p>
+            </div>
           </div>
-          <div className={styles.product_slider_slide}>
-            <h3>6</h3>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/staking.png"
+                  width={59}
+                  height={56}
+                  alt="Staking icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Staking</p>
+            </div>
+          </div>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/trading.png"
+                  width={49}
+                  height={49}
+                  alt="Trading icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Trading</p>
+            </div>
+          </div>
+          <div className={styles.product_right_item_mobile} style={{"width": "220px", "height": "178px"}}>
+            <div className={styles.product_right_item_content} style={{"width": "195px", "height": "178px"}}>
+              <div className={styles.product_right_icon}>
+                <Image 
+                  src="/images/launchpad.png"
+                  width={52}
+                  height={52}
+                  alt="Launchpad icon"
+                />
+              </div>
+              <p className={styles.product_right_caption}>Launchpad</p>
+            </div>
           </div>
         </Slider>
       </div>
