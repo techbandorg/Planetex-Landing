c8ddd39f63b3ac36fae8d37b884cc23ee3e24b7f
Alexander <alexpryp@gmail.com>
2022-10-12 18:28:03 +0300

    REF | Move the CountdownSection component to a separate module

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header/Header'
 import FirstSection from '../components/FirstSection/FirstSection'
+import CountdownSection from '../components/CountdownSection/CountdownSection'
 import ProductSlider from '../components/ProductSlider/ProductSlider'
 import RoadmapSlider from '../components/RoadmapSlider/RoadmapSlider'
 import TeamSlider from '../components/TeamSlider/TeamSlider'
@@ -22,77 +23,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <FirstSection />
-        
-        <section className={styles.win_countdown_section}>
-          <div className={styles.win_countdown_timer_block}>
-            <div className={styles.win_countdown_timer_item}>
-              <div className={styles.win_countdown_timer_cell}>1</div>
-              <div className={styles.win_countdown_timer_caption}>Days</div>
-            </div>
-            <div className={styles.win_countdown_timer_item}>
-              <div className={styles.win_countdown_timer_cell}>21</div>
-              <div className={styles.win_countdown_timer_caption}>Hours</div>
-            </div>
-            <div className={styles.win_countdown_timer_item}>
-              <div className={styles.win_countdown_timer_cell}>46</div>
-              <div className={styles.win_countdown_timer_caption}>Minutes</div>
-            </div>
-            <div className={styles.win_countdown_timer_item}>
-              <div className={styles.win_countdown_timer_cell}>11</div>
-              <div className={styles.win_countdown_timer_caption}>Seconds</div>
-            </div>
-          </div>
-          <div className={styles.win_countdown_progress_block}>
-            <div className={styles.win_countdown_progress_container}>
-              <div className={styles.win_countdown_progress_caption}>
-                Pre-sale
-              </div>
-              <div className={styles.win_countdown_progress_initial}>
-                <div className={styles.win_countdown_progress_initial_caption}>Initial sales amount:</div>
-                <div className={styles.win_countdown_progress_initial_amount}>0,04$ per token</div>
-              </div>
-              <div className={styles.win_countdown_progress_networks}>
-                <div className={styles.win_countdown_progress_network_button}>
-                  Ethereum Network
-                </div>
-                <div className={styles.win_countdown_progress_network_button}>
-                  BSC Network
-                </div>
-              </div>
-              <div className={styles.win_countdown_progress_bar}>
-                <div className={styles.win_countdown_progress_external}>
-                  <div className={styles.win_countdown_progress_internal}>
+        <CountdownSection />
 
-                  </div>
-                </div>
-              </div>
-              <div className={styles.win_countdown_progress_range}>
-                <div className={styles.win_countdown_progress_range_left}>
-                  <div>0</div>
-                  <div className={styles.win_countdown_progress_picon}>
-                    <Image
-                      src="/images/Picon.svg"
-                      height={17}
-                      width={17}
-                      alt="Planetex token icon"
-                    />
-                  </div>
-                </div>
-                <div className={styles.win_countdown_progress_range_right}>
-                  <div>28 000 000</div>
-                  <div className={styles.win_countdown_progress_picon}>
-                    <Image
-                      src="/images/Picon.svg"
-                      height={17}
-                      width={17}
-                      alt="Planetex token icon"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         <section className={styles.benefits_section}>
           <div className={styles.benefits_container}>
             <h2 className={styles.section_title}>
