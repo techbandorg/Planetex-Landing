abf6613b6582d95affff7d94ca645da333f8c76f
Alexander <alexpryp@gmail.com>
2022-10-13 14:49:54 +0300

    EDIT | uncomment FirstSection module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header/Header'
-//import FirstSection from '../components/FirstSection/FirstSection'
+import FirstSection from '../components/FirstSection/FirstSection'
 import CountdownSection from '../components/CountdownSection/CountdownSection'
 import BenefitsSection from '../components/BenefitsSection/BenefitsSection'
 import ProductSection from '../components/ProductSection/ProductSection'
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <main className={styles.main}>
-        {/* <FirstSection /> */}
+        <FirstSection />
         <CountdownSection />
         <BenefitsSection />
         <ProductSection />
