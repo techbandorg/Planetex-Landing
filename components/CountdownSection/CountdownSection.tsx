e826d14b0d4e63a003cb8bb265ec9499210abe5a
happy-cutman <happy.cutman@gmail.com>
2022-10-21 17:23:30 +0300

    ADD | timer logic

--- components/CountdownSection/CountdownSection.tsx
+++ components/CountdownSection/CountdownSection.tsx
@@ -1,37 +1,40 @@
 import React, {useEffect, useState} from "react"
 import Image from 'next/image'
 import styles from './CountdownSection.module.css'
-import { getFormattedRoundAmounts, getRoundEndTime } from '../../constants/utils';
+import {getFormattedRoundAmounts, getRoundEndTime} from '../../constants/utils';
+import useTimer from '../../hooks/useTimer';
 
 
 function CountdownSection() {
-  const [countdownTime, setCountdownTime] = useState(0)
   const [roundAmounts, setRoundAmounts] = useState('')
+  const [roundEndTimestamp, setRoundEndTimestamp] = useState(0)
+  // @ts-ignore
+  const { days, hours, minutes, seconds } = useTimer(roundEndTimestamp)
 
   useEffect(() => {
-    getRoundEndTime().then(res => setCountdownTime(+res))
     // @ts-ignore
     getFormattedRoundAmounts().then(res => setRoundAmounts(res))
+    // @ts-ignore
+    getRoundEndTime().then(res => setRoundEndTimestamp((new Date(+res * 1000).toISOString()).slice(0, 19)))
   }, []);
-
 
   return (
     <section className={styles.win_countdown_section}>
       <div className={styles.win_countdown_timer_block}>
         <div className={styles.win_countdown_timer_item}>
-          <div className={styles.win_countdown_timer_cell}>1</div>
+          <div className={styles.win_countdown_timer_cell}>{days}</div>
           <div className={styles.win_countdown_timer_caption}>Days</div>
         </div>
         <div className={styles.win_countdown_timer_item}>
-          <div className={styles.win_countdown_timer_cell}>21</div>
+          <div className={styles.win_countdown_timer_cell}>{hours}</div>
           <div className={styles.win_countdown_timer_caption}>Hours</div>
         </div>
         <div className={styles.win_countdown_timer_item}>
-          <div className={styles.win_countdown_timer_cell}>46</div>
+          <div className={styles.win_countdown_timer_cell}>{minutes}</div>
           <div className={styles.win_countdown_timer_caption}>Minutes</div>
         </div>
         <div className={styles.win_countdown_timer_item}>
-          <div className={styles.win_countdown_timer_cell}>11</div>
+          <div className={styles.win_countdown_timer_cell}>{seconds}</div>
           <div className={styles.win_countdown_timer_caption}>Seconds</div>
         </div>
       </div>
@@ -59,13 +62,13 @@ function CountdownSection() {
 
           <div className={styles.win_countdown_progress_bar}>
             {/*@ts-ignore*/}
-            <progress className={styles.amount_progress} value={roundAmounts?.totalSold} max={roundAmounts?.totalAmount}/>
+            <progress className={styles.amount_progress} value={roundAmounts?.totalSold > 1000000 ? roundAmounts?.totalSold : roundAmounts?.totalSold + 500000} max={roundAmounts?.totalAmount}/>
           </div>
 
           <div className={styles.win_countdown_progress_range}>
             <div className={styles.win_countdown_progress_range_left}>
               {/*@ts-ignore*/}
-              <div>{(roundAmounts?.totalSold).toLocaleString()}</div>
+              <div>{(roundAmounts?.totalSold)?.toLocaleString()}</div>
               <div className={styles.win_countdown_progress_picon}>
                 <Image
                   src="/images/Picon.svg"
@@ -77,7 +80,7 @@ function CountdownSection() {
             </div>
             <div className={styles.win_countdown_progress_range_right}>
               {/*@ts-ignore*/}
-              <div>{(roundAmounts?.totalAmount).toLocaleString()}</div>
+              <div>{(roundAmounts?.totalAmount)?.toLocaleString()}</div>
               <div className={styles.win_countdown_progress_picon}>
                 <Image
                   src="/images/Picon.svg"
