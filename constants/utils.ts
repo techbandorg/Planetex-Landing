f8efedf62dab28d3e907dc31bb5c1470248d71ba
happy-cutman <happy.cutman@gmail.com>
2022-10-31 14:41:18 +0200

    ADD | main contracts logic

--- constants/utils.ts
+++ constants/utils.ts
@@ -1,7 +1,7 @@
 import { getTokenSaleContract } from './contracts';
 
 export const getRoundEndTime = async () => {
-  const chainId = 97
+  const chainId = 1
   const contract = await getTokenSaleContract(chainId)
   return await contract.methods.getRoundEndTime(0).call()
 };
@@ -13,34 +13,13 @@ export const getRoundAmounts = async (chainId: number ) => {
 }
 
 export const getFormattedRoundAmounts = async () => {
-  const { totalPurchaseAmount: usdtTotalPurchaseAmount, tokensSold: usdtTokensSold } = await getRoundAmounts(5)
-  const { totalPurchaseAmount, tokensSold } = await getRoundAmounts(97)
+  const { totalPurchaseAmount: usdtTotalPurchaseAmount, tokensSold: usdtTokensSold } = await getRoundAmounts(1)
+  const { totalPurchaseAmount, tokensSold } = await getRoundAmounts(56)
+  const purchaseSum = (BigInt(totalPurchaseAmount) + BigInt(usdtTotalPurchaseAmount)).toString()
+  const soldSum = (BigInt(tokensSold) + BigInt(usdtTokensSold)).toString()
 
-  function findSum(first: string, second: string) {
-    let sum = '';
-    let carry = 0;
-    const diff = second.length - first.length;
-    for (let i = first.length - 1; i >= 0; i--) {
-      const temp =
-        (Number(first.charAt(i)) % 10) +
-        (Number(second.charAt(i + diff)) % 10) +
-        carry;
-      if (temp >= 10) {
-        sum = (temp % 10) + sum;
-        carry = Math.floor(temp / 10);
-      } else {
-        sum = temp + sum;
-        carry = 0;
-      }
-    }
-    if (carry) {
-      sum = carry + sum;
-    }
-    return sum;
-  }
-
-  const totalAmount = (+findSum(totalPurchaseAmount, usdtTotalPurchaseAmount) / 10 ** 18)
-  const totalSold = (+findSum(usdtTokensSold, tokensSold) / 10 ** 18)
+  const totalAmount = +purchaseSum / 10 ** 18
+  const totalSold = +soldSum / 10 ** 18
 
   return { totalAmount, totalSold }
 };
